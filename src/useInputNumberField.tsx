8561157995926de605dee06971ec10ba949ededc
Travis Harmon <harmtrav@gmail.com>
2019-12-18 21:54:31 -0500

    add select element to input number field

--- src/useInputNumberField.tsx
+++ src/useInputNumberField.tsx
@@ -1,8 +1,8 @@
 import useField, { UseFieldArgs, Field } from './useField'
 
-export type InputNumberField = Field<number | undefined, HTMLInputElement>
+export type InputNumberField = Field<number | undefined, HTMLInputElement | HTMLSelectElement>
 
-const event2value = (e: React.ChangeEvent<HTMLInputElement>) => parseFloat(e.target.value)
+const event2value = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => parseFloat(e.target.value)
 
 export default function useInputNumberField(args: UseFieldArgs<number | undefined>) {
     return useField(args, event2value)
