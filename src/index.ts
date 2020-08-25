2b5fa716f7176f3822c170f371add7c28c409b52
Travis Harmon <twharmon@gmail.com>
2020-08-25 15:08:44 -0500

    Rework exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-export * from './Valid'
-export * from './useField'
-export * from './useInputField'
-export * from './useInputNumberField'
-export * from './useMomentField'
-export * from './useCheckBoxField'
-export * from './useForm'
+import { Valid } from './Valid'
+import useField, { Field as FieldType } from './useField'
+import useInputField, { InputField as InputFieldType } from './useInputField'
+import useInputNumberField, { InputNumberField as InputNumberFieldType } from './useInputNumberField'
+import useMomentField, { MomentField as MomentFieldType } from './useMomentField'
+import useCheckBoxField, { CheckBoxField as CheckBoxFieldType } from './useCheckBoxField'
+import useForm from './useForm'
+
+export {
+    Valid,
+    useForm,
+    useField,
+    useInputField,
+    useInputNumberField,
+    useMomentField,
+    useCheckBoxField,
+}
+
+export type Field<ValueType, ElementType> = FieldType<ValueType, ElementType>
+export type InputField = InputFieldType
+export type InputNumberField = InputNumberFieldType
+export type MomentField = MomentFieldType
+export type CheckBoxField = CheckBoxFieldType
+
